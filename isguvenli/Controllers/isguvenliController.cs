8bcfa349be6af76159960b51dd3da86b08daa268
yasar360 <yasar360@BPC05>
2024-02-22 10:04:18 +0300

    Proje dosyası ekle.

---
+++ isguvenli/Controllers/isguvenliController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace isguvenli.Controllers
+{
+    public class isguvenliController : Controller
+    {
+        public IActionResult isguvenli()
+        {
+            return View();
+        }
+
+        
+
+
+
+        [Route("isguvenli/Liste/{id}")]
+        public string Liste(int id) {
+            return $"parametrenin gsterdiği değer:{id}";
+        }
+    }
+}
---
+++ isguvenli/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace isguvenli.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
